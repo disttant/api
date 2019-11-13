3dd5c4520807c531c23191eaf6a1225d7acd1b4f
dalher <donfumero@gmail.com>
2019-11-13 19:39:45 +0000

    Bugfix: Relation had not fillable fields

--- app/Relation.php
+++ app/Relation.php
@@ -8,6 +8,14 @@ class Relation extends Model
 {
     //
     protected $primaryKey = 'user_id';
+    
     public $incrementing = false;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'channel_id',
+        'group_id'
+    ];
     
 }
